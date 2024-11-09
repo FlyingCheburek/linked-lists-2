d2b9add1a1ce533f188a949895eb35ba2aaac4d0
FlyingCheburek <thomashenriquesantospinheiro@gmail.com>
2024-11-09 19:00:19 -0300

    feat: update singly_linked.hpp

--- src/main/linked_list/singly_linked.hpp
+++ src/main/linked_list/singly_linked.hpp
@@ -1,16 +1,31 @@
 #include <forward_list>
+#include <functional>
+#include <stdexcept>
 
 template<class T>
-struct Node {
+struct SinglyNode {
     T data;
-    Node<T>* next;
+    SinglyNode<T>* next = nullptr;
+    SinglyNode() noexcept {  }
+    SinglyNode(const T data, SinglyNode<T>* next) noexcept : data(data), next(next) {  } 
 };
 
 template<class T>
 class SinglyLinked {
 protected:
-    Node<T>* head;
+    SinglyNode<T>* head = nullptr;
 
 public:
     SinglyLinked() noexcept {  }
+
+    void for_each(std::function<void(T item)> func) const noexcept {
+        for (SinglyNode<T>* node = head; node; node = node->next) func(node->data);
+    }
+    T front() const {  
+        if (!head) throw std::out_of_range("Cannot acess 'head' pointer from an empty list.");
+        return head->data;
+    }
+    void push_front(const T item) noexcept {
+        head = new SinglyNode<T>(item, head);
+    }
 };
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,6 +3,15 @@
 
 int main() {
     SinglyLinked<int> list;
-    
+    try {
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+        list.push_front(4);
+        list.push_front(5);
+        
+    } catch (std::exception err) {
+        std::cerr << err.what();
+    }
     return 0;
 }
