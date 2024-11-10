2b909872049329697a104bd8a32f4272b62c0f39
FlyingCheburek <thomashenriquesantospinheiro@gmail.com>
2024-11-09 23:05:22 -0300

    feat: basic operations for the SLL

--- src/main/linked_list/singly_linked.hpp
+++ src/main/linked_list/singly_linked.hpp
@@ -1,31 +1,90 @@
-#include <forward_list>
+#include <vector>
 #include <functional>
 #include <stdexcept>
+#include <iostream>
+
+#ifndef SINGLY_LINKED_HPP
 
 template<class T>
 struct SinglyNode {
     T data;
     SinglyNode<T>* next = nullptr;
     SinglyNode() noexcept {  }
-    SinglyNode(const T data, SinglyNode<T>* next) noexcept : data(data), next(next) {  } 
+    SinglyNode(const T data, SinglyNode<T>* next = nullptr) noexcept : data(data), next(next) {  } 
 };
 
 template<class T>
 class SinglyLinked {
 protected:
     SinglyNode<T>* head = nullptr;
+    SinglyNode<T>* get_back() const noexcept {
+        if (empty()) return nullptr;
+        SinglyNode<T>* back = head;
+        while (back->next) back = back->next;
+        return back;
+    }
 
 public:
     SinglyLinked() noexcept {  }
+    SinglyLinked(const std::vector<T>&& items) noexcept {
+        for (auto it = items.rbegin(); it != items.rend(); it++) {
+            push_front(*it);
+        }
+    }
+    SinglyLinked(const std::vector<T>& items) noexcept {
+        for (auto it = items.rbegin(); it != items.rend(); it++) {
+            push_front(*it);
+        }
+    }
+    ~SinglyLinked() {
+        for (SinglyNode<T>* node = head, *temp; node;) {
+            temp = node;
+            node = node->next;
+            delete temp;
+        }
+    }
 
+    inline bool empty() const noexcept {
+        return head == nullptr;
+    }
     void for_each(std::function<void(T item)> func) const noexcept {
         for (SinglyNode<T>* node = head; node; node = node->next) func(node->data);
     }
     T front() const {  
-        if (!head) throw std::out_of_range("Cannot acess 'head' pointer from an empty list.");
+        if (empty()) throw std::out_of_range("Cannot acess 'head' pointer from an empty list.");
         return head->data;
+    }
+    T back() const {
+        if (empty()) throw std::out_of_range("Cannot acess 'head' pointer from an empty list.");
+        return get_back()->data;
     }
     void push_front(const T item) noexcept {
         head = new SinglyNode<T>(item, head);
     }
+    void push_back(const T item) noexcept {
+        SinglyNode<T>* back = get_back();
+        if (!back) head = new SinglyNode<T>(item);
+        else back->next = new SinglyNode<T>(item);
+    }
+    void pop_front() noexcept {
+        if (empty()) return;
+        SinglyNode<T>* old = head;
+        head = head->next;
+        delete old;
+    }
+    void pop_back() noexcept {
+        if (empty()) return;
+        if (!head->next) pop_front();
+        else {
+            SinglyNode<T>* pred = head, *curr = head->next;
+            while (curr->next) {
+                pred = curr;
+                curr = curr->next;
+            }
+            delete curr;
+            pred->next = nullptr;
+        }
+    }
 };
+
+#endif
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,17 +1,6 @@
-#include <iostream>
 #include "linked_list/singly_linked.hpp"
 
 int main() {
-    SinglyLinked<int> list;
-    try {
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
-        list.push_front(4);
-        list.push_front(5);
-        
-    } catch (std::exception err) {
-        std::cerr << err.what();
-    }
+    SinglyLinked<int> list({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
     return 0;
 }
